2654e4caffe54986cd11c3ffdc13dcd1a6fcdd5d
Luis Fernando <luis_santos.02@hotmail.com>
2021-05-22 18:08:22 -0300

    Criado o script do condicional case

---
+++ shell-scripts/case.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Comando Case
+case $1 in
+1) echo "Um" ;;
+2) echo "Dois" ;;
+3) echo "Tres" ;;
+4) echo "Quatro" ;;
+5) echo "Cinco" ;;
+6) echo "Seis" ;;
+7) echo "Sete" ;;
+8) echo "Oito" ;;
+9) echo"Nove" ;;
+10) echo "Dez" ;;
+*) echo "Opcao Invalida!" ;;
+esac
+
